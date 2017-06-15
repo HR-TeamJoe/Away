ecc3f48a46096fed70a0d13dcfc13db4bfeca713
Rob Cornell <rob.cornell@gmail.com>
2017-06-15 14:23:04 -0400

    Update files to render home page

---
+++ public/src/components/app.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>I would like to go somewhere...</h3>
+        <span>
+          <span>*Dropdown*</span>
+          <span>around</span>
+          <span>*Calendar*</span>
+          <form>
+            <button >Go!</button>
+          </form>
+        </span>
+      </div>
+    )
+  }
+}
+
+module.exports = App;
