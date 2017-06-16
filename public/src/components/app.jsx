387b4acef918d526bc61beb83e21da2ae5306672
Hhenry88 <henryhan88@outlook.com>
2017-06-16 08:57:15 -0400

    temp and date props moved to app.jsx into stateless, added simple button functionality for now

--- public/src/components/app.jsx
+++ public/src/components/app.jsx
@@ -1,24 +1,57 @@
 import React from 'react';
 import Calendar from './calendar.jsx';
 import TempDropdown from './tempDropdown.jsx';
+import moment from 'moment';
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      startDate: moment(),
+      temp: 'warm'
+    }
+
+    this.changeTemp = this.changeTemp.bind(this);
+    this.changeDate = this.changeDate.bind(this);
+  }
+
+  changeTemp(e) {
+    this.setState({
+      temp: e.target.value
+    })
+  }
+
+  changeDate(date){
+    this.setState({
+      startDate: date
+    })
+  }
+
+  getCityResults(e){
+    e.preventDefault();
+    console.log(this.state.startDate, this.state.temp);
+
+    //invoke a method yet to be made that posts to /api/search. that will begin getCityResults process
   }
 
   render() {
     return (
       <div>
         <h3>I would like to go somewhere...</h3>
-        <span>
-          <span><TempDropdown options={['hot', 'warm', 'crisp', 'cold', 'freezing']}/></span>
+        <div>
+          <span>
+            <TempDropdown options={['hot', 'warm', 'crisp', 'cold', 'freezing']} changeTemp={this.changeTemp} temp={this.state.temp}/>
+          </span>
           <span>around</span>
-          <span><Calendar /></span>
+          <span>
+            <Calendar startDate={this.state.startDate} changeDate={this.changeDate}/>
+          </span>
           <form>
-            <button >Go!</button>
+            <button onClick={(e) => this.getCityResults(e)}>Go!</button>
           </form>
-        </span>
+        </div>
       </div>
     )
   }
--- public/src/components/tempDropdown.jsx
+++ public/src/components/tempDropdown.jsx
@@ -1,34 +1,18 @@
 import React from 'react'
 
-class TempDropdown extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			value: ''
-		}
-		this.handleChange = this.handleChange.bind(this)
-	}
-
-	handleChange(e) {
-		this.setState(
-			{value: e.target.value}
-		)
-	}
-
-	render() {
-		return (
-			<div>
-				<select onChange={this.handleChange} value={this.state.value}>
-					{this.props.options.map((option, i) => {
-						return (
-							<option key={i} value={option}>{option}</option>
-						)
-					})}
-				</select>
-			</div>
-		)
-	}
-}
+var TempDropdown = (props) => (
+		<div>
+			<select onChange={props.changeTemp} value={props.temp}>
+				{
+          props.options.map((option, i) => {
+					  return (
+		          <option key={i} value={option}>{option}</option>
+					  )
+				  })
+        }
+			</select>
+		</div>
+)
 
 // TempDropdown.propTypes: {
 // 	options: React.PropTypes.any.isRequired,
