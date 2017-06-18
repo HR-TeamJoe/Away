344766149c7e11ed06480eae6b5af779b3e45d2e
Rob Cornell <rob.cornell@gmail.com>
2017-06-18 16:52:43 -0400

    Finish user auth in local server

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 var express = require('express');
-var session = require('express-session');
 var parser = require('body-parser');
 var morgan = require('morgan');
 var router = require('./server/router/router.js');
 var path = require('path');
 var passport = require('passport');
-var cookieParser = require('cookie-parser');
+var db = require('./db/models/cityModel.js');
 var googleAuthConfig = require('./googleAuthConfig.js');
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
-
-var app = express();
-
 
 //CREATE FUNCTION TO SET KEYS
 var googleClientId = googleAuthConfig.googleClientId;
@@ -19,9 +15,6 @@ var googleCallbackUrl = "http://localhost:1337/auth/google/callback";
 
 
 //GOOGLE AUTH SETUP****************************
-
-
-
 passport.use(new GoogleStrategy({
     clientID: googleClientId,
     clientSecret: googleClientSecret,
@@ -31,8 +24,7 @@ passport.use(new GoogleStrategy({
     // User.findOrCreate({ googleId: profile.id }, function (err, user) {
       console.log('This is where you would save the user to DB. Profile is: ', profile);
       // return cb(err, user);
-      var user = { googleId: profile.googleId };
-      return cb(null, user);
+      return cb(null, profile);
     // });
   }
 ));
@@ -47,18 +39,17 @@ passport.deserializeUser(function(obj, cb) {
 
 //GOOGLE AUTH SETUP****************************
 
-var db = require('./db/models/cityModel.js');
+var app = express();
 
-app.use(morgan('dev'));
-app.use(parser.json());
-app.use(parser.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'tonks', resave: true, saveUninitialized: true }));
 app.use((req, res, next) => {
-  console.log('Logging session:', req.session);
+  console.log('SESSION: ', req.session);
   next();
-})
+});
 
-app.use(session({secret: 'tonks', resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -67,17 +58,15 @@ app.use(express.static(path.resolve(__dirname, './public')));
 
 app.use('/api', router);
 
-//GOOGLE AUTH SETUP****************************
 app.get('/auth/google',
   passport.authenticate('google', { scope: ['profile'] }));
 
 app.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/login' }), //HAVE TO CHANGE THIS
+  passport.authenticate('google', { failureRedirect: '/login' }), //HAVE TO CHANGE THIS REDIRECT URL
   function(req, res) {
     // Successful authentication, redirect home.
     res.redirect('/');
   });
-//GOOGLE AUTH SETUP****************************
 
 
 app.set('port', process.env.PORT || 1337);
