db5f2217c335e10ade4e71e305d5f0dbb3934c11
Rob Cornell <rob.cornell@gmail.com>
2017-06-20 10:12:42 -0400

    Result data populates tabs (dummy data)

--- public/src/components/resultTabs.jsx
+++ public/src/components/resultTabs.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+// import 'react-tabs/style/react-tabs.css';
 
 var ResultTabs = (props) => (
 
   <Tabs>
     <TabList>
-      <Tab>Title 1</Tab>
-      <Tab>Title 2</Tab>
+      {props.destinations.map((destination, idx) => {
+        return <Tab key={idx}>{destination.city.city}</Tab>;
+      })}
     </TabList>
-
-    <TabPanel>
-      <h2>Any content 1</h2>
-    </TabPanel>
-    <TabPanel>
-      <h2>Any content 2</h2>
-    </TabPanel>
+    {props.destinations.map((destination, idx) => {
+      return (
+        <TabPanel key={idx}>
+          {destination.googleData.results.map((location, locIdx) => {
+            return <div><a key={locIdx} href="http://www.google.com">{location.name}</a></div>;
+          })}
+        </TabPanel> 
+      )
+    })}
   </Tabs>
 
 );
