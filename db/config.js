acdf8dae0343f988804c9941ce409e93d6b5dc5b
Rob Cornell <rob.cornell@gmail.com>
2017-06-26 13:34:43 -0400

    More linting

--- db/config.js
+++ db/config.js
@@ -1,24 +1,23 @@
-var Rebase = require('re-base');
-var firebase = require('firebase');
-// var app;
+const Rebase = require('re-base');
+const firebase = require('firebase');
+// const app;
 
 // console.log('PROCESS.ENV IS: ', process.env);
 // console.log('firebaseAPI_KEY is: ', process.env.firebaseAPI_KEY);
 
-var API_KEY = 'AIzaSyC1nsBCq-D1-I3gysxeWUDfRnOGIBQNeU8';
-// var API_KEY = process.env.FIREBASE_API_KEY;
+const API_KEY = 'AIzaSyC1nsBCq-D1-I3gysxeWUDfRnOGIBQNeU8';
+// const API_KEY = process.env.FIREBASE_API_KEY;
 // console.log('API_KEY is now: ', API_KEY);
 
 // if ( API_KEY ) {
-var app = firebase.initializeApp({
+const app = firebase.initializeApp({
   apiKey: API_KEY,
-  authDomain: "hraway-36d46.firebaseapp.com",
-  databaseURL: "https://hraway-36d46.firebaseio.com",
-  projectId: "hraway-36d46",
-  storageBucket: "hraway-36d46.appspot.com",
-  messagingSenderId: "802087692134"
+  authDomain: 'hraway-36d46.firebaseapp.com',
+  databaseURL: 'https://hraway-36d46.firebaseio.com',
+  projectId: 'hraway-36d46',
+  storageBucket: 'hraway-36d46.appspot.com',
+  messagingSenderId: '802087692134'
 });
-  
 // } else {
 //   API_KEY = require('./config.keys.js');
 //   app = firebase.initializeApp({
@@ -31,6 +30,6 @@ var app = firebase.initializeApp({
 //   });
 // }
 
-var db = Rebase.createClass(app.database());
+const db = Rebase.createClass(app.database());
 
 module.exports = db;
