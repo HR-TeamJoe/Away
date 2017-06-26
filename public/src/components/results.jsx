08fa2432ea43a178532cd933c2efde3a904e3317
ACS207 <acshih@gmail.com>
2017-06-26 17:03:48 -0400

    Format date on user search history and historical search results

--- public/src/components/results.jsx
+++ public/src/components/results.jsx
@@ -3,26 +3,32 @@ import MapView from './mapView.jsx';
 import DestinationsList from './resultBoxes.jsx';
 import ResultTabs from './resultTabs.jsx';
 import ResultsSummary from './resultsSummary.jsx';
-
-
+import moment from 'moment';
 
 class Results extends React.Component {
   constructor(props) {
     super(props);
   }
-  
+
   render() {
+    var dateForFlight;
+    if ( typeof this.props.date === 'function') {
+      dateForFlight = this.props.date;
+    } else {
+      dateForFlight = moment(this.props.date);
+    }
+
     return (
     <div className="results-container">
       <div className="results">
-        <ResultsSummary temp={this.props.temp} date={this.props.date} budget={this.props.budget} interests={this.props.interests}/>
+        <ResultsSummary temp={this.props.temp} date={dateForFlight} budget={this.props.budget} interests={this.props.interests}/>
         <DestinationsList destinations={this.props.results} changeCity={this.props.changeCity} selectedCity={this.props.selectedCity}/>
         <MapView destinations={this.props.results}/>
         <ResultTabs startDate={this.props.date} destinations={this.props.results} changeCity={this.props.changeCity} selectedCity={this.props.selectedCity}/>
       </div>
     </div>
     )
- } 
+ }
 }
 
 export default Results;
